062be9dbdb41d5985a1d3676a550054ca8c90823
Ayman Adlan <aymanadlan@hotmail.com>
2024-08-27 21:45:27 +0400

    add ProductCart component and fix tailwind.config.js bug (add tsx extenstion)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ProductCart from "./components/ProductCart";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div className="bg-indigo-600 m-5 grid grid-cols-4  p-2 rounded-md">
+        <ProductCart />
+        <ProductCart />
+        <ProductCart />
+        <ProductCart />
+        <ProductCart />
+        <ProductCart />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
